b535908083848a4fe574e7f5ec0b7ce4f6acd1b1
Priyanshu Vishwakarma <priyanshuvish.3279@gmail.com>
2026-05-27 15:39:06 +0530

    Refactor variables in dynamodb module

--- modules/dynamoDB/variables.tf
+++ modules/dynamoDB/variables.tf
@@ -1,14 +1,12 @@
 variable "dynamodb_table_name" {
     description = "This variable holds the database table name"
     default = "practice_remote_table"
-    type = string
-  
+    type = string 
 }
 
 variable "env"{
     description = "This holds the environment name"
     type = string
-    # default = "dev"
 }
 variable "dynamodb_table_count" {
   description = "This value holds dynamodb table count"
